831d23be551aecc7e9c95aa5cc5e95f8b68d005f
F van Gastel <felicia.vangastel@student.uva.nl>
2018-06-14 16:08:38 +0200

    finally good data!

--- data/convertCSV2JSON.py
+++ data/convertCSV2JSON.py
@@ -12,31 +12,33 @@ import csv
 import json
 from collections import OrderedDict
 
-csvfile = open("total-greenhouse-gas-emissions.csv", "r", encoding='utf-8-sig')
-jsonfile = open("total-greenhouse-gas-emissions.json", "w")
+with open("total-greenhouse-gas-emissions.csv", "r", encoding='utf-8-sig') as csvfile:
 
-fieldnames = ("name", "id", "1960", "1961", "1962", "1963", "1964",
-    "1965", "1966", "1967", "1968", "1969", "1970", "1971", "1972", "1973", "1974",
-    "1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984",
-    "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994",
-    "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004",
-    "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014")
-reader = csv.DictReader(csvfile, fieldnames, delimiter=";")
+    jsonfile = open("total-greenhouse-gas-emissions.json", "w")
 
-list = []
+    fieldnames = ("name", "id", "1970", "1971", "1972", "1973", "1974",
+        "1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984",
+        "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994",
+        "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004",
+        "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012")
 
-for row in reader:
-    data = OrderedDict()
-    data["name"] = row["name"]
-    data["id"] = row["id"]
+    reader = csv.DictReader(csvfile, fieldnames, delimiter=";")
+    next(reader)
 
-    values = []
-    for year in fieldnames[2:]:
-        values.append({"year": year, "emission": row[year]})
-    data["values"] = values
-    list.append(data)
+    list = []
 
-json.dump(list, jsonfile, indent=4)
+    for row in reader:
+        data = OrderedDict()
+        data["name"] = row["name"]
+        data["id"] = row["id"]
 
-csvfile.close()
-jsonfile.close()
+        values = []
+        for year in fieldnames[2:]:
+            values.append({"year": year, "emission": row[year]})
+        data["values"] = values
+        list.append(data)
+
+    json.dump(list, jsonfile, indent=4)
+
+    csvfile.close()
+    jsonfile.close()
